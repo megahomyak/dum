0c8d84dac8f26fdf16738113b0f87eb9096b5e03
megahomyak <g.megahomyak@gmail.com>
2024-10-05 03:35:42 +0500

    Wanted to do good error handling, but fuck it, this is C, error handling here will be crap no matter what

--- dum.c
+++ dum.c
@@ -1,10 +1,17 @@
 #include <pthread.h>
 #include <signal.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/socket.h>
 
 #define HANDLER_THREAD_COUNT 20
 
 #define NO_JOB -1
 #define SHOULD_STOP -2
+
+#define PORT "80"
+
+#define DIE_AND(new_exit_code, label) { exit_code = new_exit_code; goto label; }
 
 pthread_mutex_t current_job_guard;
 pthread_cond_t current_job_changed;
@@ -32,8 +39,7 @@ void set_signal_handler(void (*handler) (int)) {
     signal(SIGTERM, handler);
 }
 
-void stop_signal_handler(int signal_identifier) {
-    (void)signal_identifier;
+void stop(void) {
     set_signal_handler(SIG_IGN);
     pthread_mutex_lock(&current_job_guard);
     current_job = SHOULD_STOP;
@@ -41,36 +47,75 @@ void stop_signal_handler(int signal_identifier) {
     pthread_mutex_unlock(&current_job_guard);
 }
 
+void stop_signal_handler(int signal_identifier) {
+    (void) signal_identifier;
+    stop();
+}
+
+int exit_code = 0;
+
 int main(void) {
     pthread_condattr_t* condition_attributes = NULL;
     pthread_mutexattr_t* mutex_attributes = NULL;
-    pthread_mutex_init(&current_job_guard, mutex_attributes);
-    pthread_cond_init(&current_job_changed, condition_attributes);
+    if (pthread_mutex_init(&current_job_guard, mutex_attributes) != 0) DIE_AND(1, exit);
+    if (pthread_cond_init(&current_job_changed, condition_attributes) != 0) DIE_AND(2, destroy_current_job_guard);
 
     set_signal_handler(stop_signal_handler);
 
     pthread_t threads[HANDLER_THREAD_COUNT];
 
     pthread_attr_t thread_attributes;
-    pthread_attr_init(&thread_attributes);
-    pthread_attr_setstacksize(&thread_attributes, 1024);
+    if (pthread_attr_init(&thread_attributes) != 0) DIE_AND(3, destroy_current_job_changed);
+    if (pthread_attr_setstacksize(&thread_attributes, 1024) != 0) DIE_AND(4, destroy_current_job_changed);
 
     for (int i = 0; i < HANDLER_THREAD_COUNT; ++i) {
         void* input = NULL;
-        pthread_create(&threads[i], &thread_attributes, worker_thread, input);
+        if (pthread_create(&threads[i], &thread_attributes, worker_thread, input) != 0) DIE_AND(4, );
     }
 
-    /* listen */
+    {
+        struct addrinfo hints;
+        memset(&hints, 0, sizeof hints);
+        hints.ai_family = AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+        hints.ai_flags = AI_PASSIVE;
 
+        struct addrinfo* result;
+        char* name = NULL;
+        if (getaddrinfo(name, PORT, &hints, &result) != 0) {
+            exit_code = 1;
+            stop();
+            goto destroy_threads;
+        }
+
+        int server_descriptor = -1;
+        for (struct addrinfo* current = result; current != NULL; current = current->ai_next) {
+
+        }
+
+        if (server_descriptor == -1) {
+            exit_code = 2;
+            stop();
+            goto destroy_threads;
+        }
+
+        freeaddrinfo(result);
+    }
+
+    destroy_threads:
     for (int i = 0; i < HANDLER_THREAD_COUNT; ++i) {
         void* output;
         pthread_join(threads[i], &output);
     }
 
+    destroy_thread_attributes:
     pthread_attr_destroy(&thread_attributes);
 
+    destroy_current_job_changed:
     pthread_cond_destroy(&current_job_changed);
+    destroy_current_job_guard:
     pthread_mutex_destroy(&current_job_guard);
 
-    return 0;
+    exit:
+    return exit_code;
 }
